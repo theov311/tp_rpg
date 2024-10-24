c90264af89f08c6d2326e62521bf9b925b4bbf41
Librqirie <137263516+Librqirie@users.noreply.github.com>
2024-10-24 15:45:02 +0200

    attack
    - si attaque alors -1 hp
    - si 0 hp alors mort, donc ne peut plus attaquer

--- main.py
+++ main.py
@@ -3,3 +3,14 @@ class Character:
         self.name = name
         self.hp = 10 
 
+def attack(self, other):
+        if self.hp > 0:
+            other.hp -= 1
+            print(f"{self.name} attaque {other.name}!")
+            if other.hp <= 0:
+                other.hp = 0
+                print(f"{other.name} est mort!")
+            else:
+                print(f"{other.name} a maintenant {other.hp} HP.")
+        else:
+            print(f"{self.name} ne peut pas attaquer car il est mort.")
