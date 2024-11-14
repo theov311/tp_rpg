5b22ddf78a2d94b6ffd856155c83c8d1d1d6c9b0
Librqirie <137263516+Librqirie@users.noreply.github.com>
2024-11-14 13:20:47 +0100

    ci.yml v1
    - dev-branch ok
    - merge-to-master ok

--- tests/test.py
+++ tests/test.py
@@ -1,9 +1,8 @@
-# tests/test_basic.py
 import unittest
 
 class BasicTest(unittest.TestCase):
     def test_example(self):
-        self.assertTrue(True)
+        self.assertTrue(False)
 
 if __name__ == '__main__':
     unittest.main()
