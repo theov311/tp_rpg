c2460dccbd16e23964b5fd54fa4c072aec662004
Librqirie <137263516+Librqirie@users.noreply.github.com>
2024-11-14 11:36:51 +0100

    Update test.py

--- tests/test.py
+++ tests/test.py
@@ -3,7 +3,7 @@ import unittest
 
 class BasicTest(unittest.TestCase):
     def test_example(self):
-        self.assertTrue(False)
+        self.assertTrue(True)
 
 if __name__ == '__main__':
     unittest.main()
