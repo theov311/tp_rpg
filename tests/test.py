27514faf36df0de3540064372505d6b26612cd64
theov311 <theo.versace@proton.me>
2024-11-14 11:49:33 +0100

    Update test.py

--- tests/test.py
+++ tests/test.py
@@ -3,7 +3,8 @@ import unittest
 
 class BasicTest(unittest.TestCase):
     def test_example(self):
-        self.assertTrue(True)
+        self.assertTrue(False)  # Ce test échouera
 
 if __name__ == '__main__':
     unittest.main()
+
